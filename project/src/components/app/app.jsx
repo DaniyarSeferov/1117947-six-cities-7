ea354121739eaf8cca004d785a01bc8730abbb06
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-06-09 00:32:57 +0300

    #Task1.3.3: Props

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainScreen from '../main-screen/main-screen';
 
-function App() {
+function App(props) {
+  const {placesCount} = props;
+
   return (
-    <MainScreen />
+    <MainScreen placesCount={placesCount} />
   );
 }
+
+App.propTypes = {
+  placesCount: PropTypes.number.isRequired,
+};
 
 export default App;

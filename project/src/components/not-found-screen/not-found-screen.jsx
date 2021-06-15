14a9b05c44fae18312f85001c565130e214969cb
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-06-15 12:09:54 +0300

    #Task2.1.3: Header component

--- project/src/components/not-found-screen/not-found-screen.jsx
+++ project/src/components/not-found-screen/not-found-screen.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
+import Header from '../header/header';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 function NotFoundScreen() {
 
   return (
     <div className="page page--gray page--not-found">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__login">Sign in</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="page__main page__main--not-found">
         <div className="page__not-found-container container">
           <section className="not-found">
             <h1 className="login__title">404 Not Found</h1>
-            <a href="main.html">Back to Home</a>
+            <Link to={AppRoute.ROOT}>Back to Home</Link>
           </section>
         </div>
       </main>

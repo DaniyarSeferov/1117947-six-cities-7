602339e83f6b89245dfb0f7881b87e6468d54250
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-06-20 21:58:11 +0300

    #Task3.8: Favorites empty

--- project/src/components/favorites-screen/favorites-screen.jsx
+++ project/src/components/favorites-screen/favorites-screen.jsx
@@ -13,7 +13,15 @@ function FavoritesScreen(props) {
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
-          <FavoritesList offers={offers} />
+          {offers.length ? <FavoritesList offers={offers} /> : (
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">Favorites (empty)</h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">Nothing yet saved.</b>
+                <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+              </div>
+            </section>
+          )}
         </div>
       </main>
       <footer className="footer container">

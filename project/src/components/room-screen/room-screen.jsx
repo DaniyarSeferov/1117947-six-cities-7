6a3b4a2e2dc7e116d664c15d94a432467c032910
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-06-24 09:58:23 +0300

    #Task3.11: Room Reviews

--- project/src/components/room-screen/room-screen.jsx
+++ project/src/components/room-screen/room-screen.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import Header from '../header/header';
 import {offerProp, reviewProp} from './room-screen.prop';
 import PropTypes from 'prop-types';
@@ -111,29 +112,36 @@ function RoomScreen(props) {
               <section className="property__reviews reviews">
                 <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
                 <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
-                      </div>
-                      <span className="reviews__user-name">
-                        Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: '80%'}}></span>
-                          <span className="visually-hidden">Rating</span>
+                  {reviews.map((review) => {
+                    const reviewRatingPercent = getRatingPercent(review.rating);
+                    const reviewDate = dayjs(review.date);
+                    return (
+                      <li key={review.id} className="reviews__item">
+                        <div className="reviews__user user">
+                          <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                            <img className="reviews__avatar user__avatar" src={review.user.avatarUrl} width="54" height="54" alt="Reviews avatar"/>
+                          </div>
+                          <span className="reviews__user-name">
+                            {review.user.name}
+                          </span>
                         </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.
-                        The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
+                        <div className="reviews__info">
+                          <div className="reviews__rating rating">
+                            <div className="reviews__stars rating__stars">
+                              <span style={{width: `${reviewRatingPercent}%`}}></span>
+                              <span className="visually-hidden">Rating</span>
+                            </div>
+                          </div>
+                          <p className="reviews__text">
+                            {review.comment}
+                          </p>
+                          <time className="reviews__time" dateTime={reviewDate.format('YYYY-MM-DD')}>
+                            {reviewDate.format('MMMM YYYY')}
+                          </time>
+                        </div>
+                      </li>
+                    );
+                  })}
                 </ul>
                 <form className="reviews__form form" action="#" method="post">
                   <label className="reviews__label form__label" htmlFor="review">Your review</label>

cfa12c89b6a5e1aa8ec842f856b2eb20893f80a0
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-07-11 12:24:31 +0300

    #Task5.1.1: Custom hooks

--- project/src/components/map/map.jsx
+++ project/src/components/map/map.jsx
@@ -1,9 +1,9 @@
 import {useEffect, useRef} from 'react';
 import 'leaflet/dist/leaflet.css';
-import useMap from './useMap';
 import PropTypes from 'prop-types';
 import leaflet from 'leaflet';
 import {locationProp} from '../room-screen/room-screen.prop';
+import useMap from '../../hooks/use-map/useMap';
 
 function Map(props) {
   const {city, points} = props;

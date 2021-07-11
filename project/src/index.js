8a5b36d609d36de387b31eca21bbbe444456b59e
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-07-11 13:54:15 +0300

    #Task5.1.4: Connect Redux DevTools

--- project/src/index.js
+++ project/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
@@ -12,7 +13,10 @@ const Setting = {
   DEFAULT_CITY: Cities.PARIS,
 };
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
